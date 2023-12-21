570092b5aa07456f25a0111e24e1ab05be57f7ff
Taichi Maruyama <maruyama@breadfish.jp>
2023-12-21 17:17:32 +0900

    fix: Exclude the build

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '81dc8bfb956af50e282f');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '2903844dd40563856d3b');
